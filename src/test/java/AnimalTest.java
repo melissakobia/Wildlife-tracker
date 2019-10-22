c875eaf9b36fa27c2eacb4c9a4af0fa7c34630eb
melissa kobia <melissakobia1997@gmail.com>
2019-10-22 04:01:21 +0300

    Adds a test file and DB rules file

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,14 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class AnimalTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+}
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,15 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+public class DatabaseRule extends ExternalResource  {
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "sonnie", "DBpassword");  //Those with linux or windows use two strings for username and password
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+
+        }
+    }
+}
