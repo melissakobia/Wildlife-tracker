0b37d58093b932674ab0321b57361dc825754bb6
melissa kobia <melissakobia1997@gmail.com>
2019-10-22 17:33:55 +0300

    Updates animal class to an abstract class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,4 +1,49 @@
-public class Animal {
-    public Animal() {
+
+import java.util.Objects;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class Animal {
+    public String name;
+    public int id;
+    public String age;
+    public static final String[] ageArray = {"newborn", "young", "adult"};
+    public String type;
+
+
+    public String getName() {
+        return name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(getName(), animal.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, age, type) VALUES (:name, :age, :type)";
+            this.id = (int) con.createQuery(sql, true).addParameter("name", this.name)
+                    .addParameter("age", this.age).addParameter("type", this.type).executeUpdate().getKey();
+        }
+    }
+
+
+
+
+
 }
