bc90cdd93df786f9d75dcb2ce82f07c3b78e7e85
melissa kobia <melissakobia1997@gmail.com>
2019-10-24 10:29:50 +0300

    Adds all current changes

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE wildlife_tracker;
 \c wildlife_tracker;
-CREATE TABLE animals (id serial PRIMARY KEY, name varchar, age varchar, type varchar);
+CREATE TABLE animals (id serial PRIMARY KEY, name varchar, age varchar,health varchar, type varchar);
 CREATE TABLE sightings(id serial PRIMARY KEY, animalId int, location varchar, rangerName varchar, sightingDate timestamp);
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
